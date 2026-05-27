3e3bd021f7083af62783d56f57a488fe031d7951
SmileyHackerz <fayemohamed82@gmail.com>
2026-05-27 11:59:00 +0000

    Lab 5 : React consomme Flask via fetch et useEffect

--- Documents/sentransport/api/app.py
+++ Documents/sentransport/api/app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -32,6 +32,54 @@ def get_ligne(id):
 
     return jsonify({"erreur": "Ligne introuvable"}), 404
 
+#Exercice1
+@app.route("/arrets")
+def get_arrets():
+    arrets = set()
+
+    for ligne in lignes:
+        for arret in ligne["listeArrets"]:
+            arrets.add(arret)
+
+    return jsonify(sorted(list(arrets)))
+
+#Exercice2
+@app.route("/stats")
+def get_stats():
+    total_lignes = len(lignes)
+
+    total_arrets = sum(ligne["arrets"] for ligne in lignes)
+
+    ligne_plus_arrets = max(lignes, key=lambda l: l["arrets"])
+
+    return jsonify({
+        "total_lignes": total_lignes,
+        "total_arrets": total_arrets,
+        "ligne_plus_arrets": {
+            "numero": ligne_plus_arrets["numero"],
+            "arrets": ligne_plus_arrets["arrets"]
+        }
+    })
+
+#Exercice3
+@app.route("/lignes/recherche")
+def rechercher_lignes():
+    q = request.args.get("q", "").lower()
+
+    resultats = []
+
+    for ligne in lignes:
+        texte = (
+            ligne["depart"] + " " +
+            ligne["arrivee"] + " " +
+            " ".join(ligne["listeArrets"])
+        ).lower()
+
+        if q in texte:
+            resultats.append(ligne)
+
+    return jsonify(resultats)
+    
 # Lancer le serveur
 if __name__ == "__main__":
     app.run(debug=True)
